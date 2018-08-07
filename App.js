fe8aff130e765e898816a33497aa796c7fdd9934
Jim Vickery <jimrv8@yahoo.com>
2018-08-06 23:52:10 -0500

    added fixed dimensions

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-class Greeting extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center', padding: 60}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
